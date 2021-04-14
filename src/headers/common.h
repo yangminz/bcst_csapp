9d09fc3a8db6d7288722ca4688c3f980ddc729af
yangmin.zhao <yangminz@users.noreply.github.com>
2021-04-14 15:49:09 +0800

    Update common.h

--- src/headers/common.h
+++ src/headers/common.h
@@ -70,4 +70,28 @@ int trie_get(trie_node_t *root, char *key, uint64_t *val);
 void trie_free(trie_node_t *root);
 void trie_print(trie_node_t *root);
 
+// extendible hash table
+typedef struct
+{
+    int localdepth;     // the local depth
+    int counter;        // the counter of slots (have data)
+    char **karray;
+    uint64_t *varray;
+} bucket_t;
+
+typedef struct
+{
+    int size;           // the size of (key, value) tuples of each bucket
+    int num;            // number of buckets = 1 << globaldepth
+    int globaldepth;    // the global depth
+
+    bucket_t *barray;    // the internal table is actually an array
+} hashtable_t;
+
+hashtable_t *hashtable_construct(int bsize);
+void hashtable_free(hashtable_t *tab);
+int hashtable_get(hashtable_t *tab, char *key, uint64_t *val);
+int hashtable_insert(hashtable_t **tab_addr, char *key, uint64_t val);
+void print_hashtable(hashtable_t *tab);
+
 #endif
