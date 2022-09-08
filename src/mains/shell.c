9f0e98989b1192a2288fb5e08be898e05b96f63a
yangmin.zhao <yangminz@users.noreply.github.com>
2022-09-08 18:03:43 +0800

    shell

--- src/mains/shell.c
+++ src/mains/shell.c
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <stdarg.h>
 
 // input line on stdin from user
 // the input line should be parsed
@@ -18,6 +19,7 @@
 #define GREENSTR(STR)   "\033[32;1m"STR"\033[0m"
 #define YELLOWSTR(STR)  "\033[33;1m"STR"\033[0m"
 #define BLUESTR(STR)    "\033[34;1m"STR"\033[0m"
+static int safe_printf(const char *fmt, ...);
 
 typedef enum
 {
@@ -47,19 +49,23 @@ typedef struct USERINPUT_STRUCT
     job_t type;
     // the count of arguments
     int argc;
-    // the value of arguments
-    char **argv;
+    // the pointers to buffer
+    char *(argv[MAX_ARG_COUNT]);
+    // the parsed buffer
+    char buffer[MAX_ARG_COUNT][MAX_LENGTH_USERINPUT];
     // the raw char pointer
     char *raw;
 } userinput_t;
 
 typedef enum
 {
-    NEW,
+    UNKNOWN,
     FG_RUNNING,
     BG_RUNNING,
     STOPPED,
 } job_state_t;
+
+static char *JOB_STATE_STR[4] = { "UK", "FG", "BG", "ST" };
 
 typedef struct JOB_TASK_STRUCT
 {
@@ -75,6 +81,7 @@ static int job_add(pid_t pid, job_state_t state, char *argstr);
 static int job_delete(pid_t pid);
 static void job_print();
 static void job_init();
+static int job_state(pid_t pid, job_state_t state);
 
 static void parse_userinput(
     const char *input,
@@ -84,19 +91,14 @@ static void evaluate(userinput_t *input);
 // handler for SIGCHLD when a child process terminates
 static void sigchld_handler(int sig);
 
-// handler for SIGINT sent by Ctrl-C
+// handler for SIGINT sent by Ctrl-C or SIGTSTP sent by Ctrl-Z
 // when Ctrl-C, the signal should be sent to foreground job group
 // i.e., child process may create child process
-// and shell process & other background processes 
-// should not be interrupted
-static void sigint_handler(int sig);
-
-// handler for SIGTSTP sent by Ctrl-Z
 // when Ctrl-Z, the signal should be sent to foreground job group
 // i.e., child process may create child process
 // and shell process & other background processes 
-// should not be stopped
-static void sigtstp_handler(int sig);
+// should not be interrupted
+static void sigproxy_handler(int sig);
 
 typedef void (sighandler_t)(int);
 sighandler_t *register_sighandler(
@@ -115,10 +117,18 @@ sighandler_t *register_sighandler(
     return old_action.sa_handler;
 }
 
-// flag indicating if sigchld has been invoked
-// to wait for child process zombies
-static int fg_reaped = 0;
-static int fg_pid;
+/*  flag indicating if sigchld has been invoked
+    to wait for child process zombies.
+    -------------------------------------------
+    Use volatile to keep the atomic flag inside memory
+    R/W global variables may invole >= 1 instructions,
+    so the operation is not atomic.
+    Use sig_atomic_t to gaurantee atomicity:
+    Write it in main flow and read it in handler flow; Or:
+    Read it in main flow and write it in handler flow.
+*/
+static volatile sig_atomic_t fg_reaped = 1;
+static volatile sig_atomic_t fg_pid = -1;
 
 int main()
 {
@@ -128,15 +138,15 @@ int main()
     // register handlers for signals
     // with this, this program can only run on *unix
     register_sighandler(SIGCHLD, sigchld_handler, SA_RESTART);
-    register_sighandler(SIGINT,  sigint_handler,  SA_RESTART);
-    register_sighandler(SIGTSTP, sigtstp_handler, SA_RESTART);
+    register_sighandler(SIGINT,  sigproxy_handler,  SA_RESTART);
+    register_sighandler(SIGTSTP, sigproxy_handler, SA_RESTART);
 
     job_init();
     pid_t shell_pid = getpid();
 
     while (1)
     {
-        printf(GREENSTR("[%d]>>>>> "), shell_pid);
+        printf(GREENSTR("[%d] > "), shell_pid);
 
         // read user input
         fgets(input_chars, MAX_LENGTH_USERINPUT, stdin);
@@ -150,18 +160,11 @@ int main()
         {
             evaluate(&input);
         }
-
-        // release the arguments
-        for (int i = 0; i < input.argc; ++ i)
-        {
-            free(input.argv[i]);
-        }
-        free(input.argv);
     }
     return 0;
 }
 
-static void run_child(userinput_t *input)
+static void child_process(userinput_t *input)
 {
     char *envp[] = { NULL };
 
@@ -171,7 +174,7 @@ static void run_child(userinput_t *input)
     // handler should delete the job from the list after it's added
     sigset_t mask, prev;
     sigemptyset(&mask);
-    sigaddset(&mask, SIGCHLD);
+    sigaddset(&mask, SIGCHLD | SIGINT | SIGTSTP);
     sigprocmask(SIG_BLOCK, &mask, &prev);
 
     pid_t pid = fork();
@@ -191,13 +194,13 @@ static void run_child(userinput_t *input)
     }
     else
     {
-        // critical section
-        // when SIGCHLD is blocked
-        // the code in this section is executed before handler
-        job_add(pid, FG_RUNNING, input->raw);
-
         if (input->type == FOREGROUND_JOB)
         {
+            // critical section
+            // when SIGCHLD is blocked
+            // the code in this section is executed before handler
+            job_add(pid, FG_RUNNING, input->raw);
+
             // fg_pid is also a critical global variable
             fg_reaped = 0;
             fg_pid = pid;
@@ -211,46 +214,64 @@ static void run_child(userinput_t *input)
                 sleep(1);
             }
         }
-        else
+        else if (input->type == BACKGROUND_JOB)
         {
-            // background job
+            // background job: just go to next iteration
+            // Question: what will happen if background job has
+            // STDOUT to terminal/monitor?
+            job_add(pid, BG_RUNNING, input->raw);
+            assert(fg_reaped = 1);
+            sigprocmask(SIG_SETMASK, &prev, NULL);
         }
     }
 }
 
 static void evaluate(userinput_t *input)
 {
-
     switch (input->type)
     {
     case BUILTIN_QUIT:
         printf(YELLOWSTR("Shell terminating ...\n"));
         exit(0);
     case BUILTIN_JOBS:
+        job_print();
         break;
     case BUILTIN_FGCONT:
         break;
     case BUILTIN_BGCONT:
         break;
     case FOREGROUND_JOB:
-        run_child(input);
-        break;
     case BACKGROUND_JOB:
+        child_process(input);
         break;
     default:
         break;
     }
 }
 
+// SIGCHLD is sent to parent whenever one child terminates
+// By default the signal is ignored so the child becomes zombie.
+// SIGCHLD handler may also be invoked when receive
+//      SIGTSTP, SIGSTOP, SIGTTIN, SIGTTOU to stop
+//      SIGCONT to resume
+// But if set signal action SA_NOCLDSTOP, will ignore
 static void sigchld_handler(int sig)
 {
     // async-signal-safe system functions may set errno
-    // when return with error. E.g., printf.
+    // when return with error. E.g., waitpid.
     // So we need to save errno on stack and restore it.
     int _errno = errno;
     int status;
 
     // waitpid(-1) -- match any process
+    // other child processes may terminate
+    // but at most one SIGCHLD would be pending
+    //
+    // waitpid - wait for state change of a child:
+    //  1.  child terminated
+    //  2.  child stopped by a signal (SIGTSTP)
+    //  3.  child resumed by a signal (CONTINUE)
+    // Wait-No-Hang flag to avoid wait and return immediately
     pid_t pid = waitpid(-1, &status, WUNTRACED | WNOHANG);
     while (pid > 0)
     {
@@ -264,49 +285,70 @@ static void sigchld_handler(int sig)
 
         if (WIFEXITED(status))
         {
+            // child was exited normally: exit() or return from main()
+            safe_printf(BLUESTR("[%u] exits normally\n"));
             job_delete(pid);
         }
 
+        if (WIFSIGNALED(status))
+        {
+            // child was terminated by signal: SIGINT
+            safe_printf(BLUESTR("[%u] is terminated by %u\n"), pid, WTERMSIG(status));
+            job_delete(pid);
+        }
+
+        if (WIFSTOPPED(status))
+        {
+            // child was stopped by signal
+            // must unset action SA_NOCLDSTOP
+            safe_printf(BLUESTR("[%u] is stopped by %u\n"), pid, WSTOPSIG(status));
+            job_state(pid, STOPPED);
+        }
+
+        if (WIFCONTINUED(status))
+        {
+            // child was resumed by delivery of SIGCONT
+            // must unset action SA_NOCLDSTOP
+            safe_printf(BLUESTR("[%u] is resumed by SIGCONT\n"), pid);
+        }
+
+        // Delivery of SIGCHLD is blocked during handler execution
+        // unless set SA_NODEFER in sigaction()
+        // If one SIGCHLD is generated again, it's marked as pending
+        // and would be delivered **WHEN HANDLER RETURNS**
         pid = waitpid(-1, &status, WUNTRACED | WNOHANG);
     }
+
+    // If another child exits here, send SIGCHLD to shell
+    // shell would have at least one pending SIGCHLD
+    // so there would be at least another handler invocation to reap.
 
     // restore errno
     errno = _errno;
 }
 
-static void sigint_handler(int sig)
+// SIGINT/SIGTSTP should be invoked only after fg_pid is valid
+static void sigproxy_handler(int sig)
 {
     int _errno = errno;
-    // proxy SIGINT to foreground process group:
+    // proxy SIGINT/SIGTSTP to foreground process group:
     // FG job may create child processes.
     // These processes are all grouped by FG job pid
     if (fg_pid > 0)
     {
-        killpg(fg_pid, SIGINT);
-        fg_pid = 0;
+        // after the child terminated by SIGINT or stopped by SIGINT
+        // waitpid will capture
+        killpg(fg_pid, sig);
     }
     errno = _errno;
 }
 
-static void sigtstp_handler(int sig)
-{
-    int _errno = errno;
-    // proxy SIGINT to foreground process group:
-    // FG job may create child processes.
-    // These processes are all grouped by FG job pid
-    if (fg_pid > 0)
-    {
-        killpg(fg_pid, SIGTSTP);
-        fg_pid = 0;
-    }
-    errno = _errno;
-}
-
+// Initialize the job list
 static void job_init()
 {
     for (int i = 0; i < MAX_NUM_JOBS; ++ i)
     {
-        job_list[i].state = NEW;
+        job_list[i].state = UNKNOWN;
         job_list[i].pid = -1;
         memset(job_list[i].argstr, '\0', sizeof(job_list[i].argstr));
     }
@@ -322,7 +364,7 @@ static int job_add(pid_t pid, job_state_t state, char *argstr)
     
     for (int i = 0; i < MAX_NUM_JOBS; ++i)
     {
-        if (job_list[i].state == NEW)
+        if (job_list[i].state == UNKNOWN)
         {
             // insert to this job
             job_list[i].pid = pid;
@@ -336,7 +378,7 @@ static int job_add(pid_t pid, job_state_t state, char *argstr)
     return 0;
 }
 
-// delete an existing pid from background job list
+// Delete an existing pid from background job list
 static int job_delete(pid_t pid)
 {
     if (pid == -1)
@@ -349,7 +391,7 @@ static int job_delete(pid_t pid)
         if (job_list[i].pid == pid)
         {
             job_list[i].pid = 0;
-            job_list[i].state = NEW;
+            job_list[i].state = UNKNOWN;
             memset(job_list[i].argstr, '\0', sizeof(job_list[i].argstr));
             return 1;
         }
@@ -359,23 +401,45 @@ static int job_delete(pid_t pid)
     return 0;
 }
 
+// Set the state of an existing pid from job list
+static int job_state(pid_t pid, job_state_t state)
+{
+    if (pid == -1)
+    {
+        return -1;
+    }
+
+    for (int i = 0; i < MAX_NUM_JOBS; ++ i)
+    {
+        if (job_list[i].pid == pid)
+        {
+            job_list[i].pid = 0;
+            job_list[i].state = state;
+            return 1;
+        }
+    }
+
+    printf(REDSTR("job_state::pid {%d} not found from job list\n"), pid);
+    return 0;
+}
+
+// Print the job list content
 static void job_print()
 {
     for (int i = 0; i < MAX_NUM_JOBS; ++ i)
     {
-        if (job_list[i].state == NEW)
+        if (job_list[i].state == UNKNOWN)
         {
             continue;
         }
-        printf("[%d]\t%d\t%s\n",
+        printf("[%d]\t%s\t%s\n",
             job_list[i].pid,
-            job_list[i].state,
+            JOB_STATE_STR[job_list[i].state],
             job_list[i].argstr);
     }
 }
 
-
-// parse the user input
+// Parse the user input
 static void parse_userinput(
     const char *input, 
     userinput_t *result)
@@ -389,8 +453,9 @@ static void parse_userinput(
     // empty as default
     result->type = EMPTY;
     result->argc = 0;
-    result->argv = NULL;
     result->raw = NULL;
+    memset(result->argv, 0, sizeof(result->argv));
+    memset(result->buffer, 0, sizeof(result->buffer));
     if (input == NULL || *input == '\0')
     {
         return;
@@ -413,9 +478,6 @@ static void parse_userinput(
         p = i;
     }
 
-    result->argv = malloc(sizeof(char *) * (result->argc + 1));
-    result->argv[result->argc] = NULL;
-
     // the current parsed argument char
     int j = 0, w = 0;
     p = -1;
@@ -424,9 +486,9 @@ static void parse_userinput(
         if ((input[i] == ' ' || input[i] == '\0' || input[i] == '\n') && 
             (p != -1 || input[p] != ' '))
         {
-            result->argv[j] = malloc(sizeof(char) * (i - w + 1));
-            strncpy(result->argv[j], &input[w], i - w);
-            result->argv[j][i - w] = '\0';
+            strncpy(result->buffer[j], &input[w], i - w);
+            result->buffer[j][i - w] = '\0';
+            result->argv[j] = result->buffer[j];
             
             // reset to next word
             j += 1;
@@ -446,9 +508,11 @@ static void parse_userinput(
     }
 
     if (result->argc >= 2 &&
-        strcmp(result->argv[result->argc - 1], "&") == 0)
+        strcmp(result->buffer[result->argc - 1], "&") == 0)
     {
         result->type = BACKGROUND_JOB;
+        result->argv[result->argc - 1] = NULL;
+        result->argc -= 1;
     }
     else if (result->argc >= 1)
     {
@@ -457,24 +521,117 @@ static void parse_userinput(
 
     if (result->argc == 1)
     {
-        if (strcmp(result->argv[0], "quit") == 0)
+        if (strcmp(result->buffer[0], "quit") == 0)
         {
             result->type = BUILTIN_QUIT;
         }
-        else if (strcmp(result->argv[0], "jobs") == 0)
+        else if (strcmp(result->buffer[0], "jobs") == 0)
         {
             result->type = BUILTIN_JOBS;
         }
     }
     else if (result->argc == 2)
     {
-        if (strcmp(result->argv[0], "fg") == 0)
+        if (strcmp(result->buffer[0], "fg") == 0)
         {
             result->type = BUILTIN_FGCONT;
         }
-        else if (strcmp(result->argv[0], "bg") == 0)
+        else if (strcmp(result->buffer[0], "bg") == 0)
         {
             result->type = BUILTIN_BGCONT;
         }
     }
+}
+
+// async-signal-safe printf: support %s and %u only
+static int convert_unsigned(unsigned u, char *buf)
+{
+    int len = 0, is_leading_zero = 1;
+    unsigned b = 1000000000, t = 0;
+    while (b > 0)
+    {
+        t = u / b;
+        u = u - t * b;
+        if (t != 0 || b == 10)
+        {
+            is_leading_zero = 0;
+        }
+        if (t != 0 || is_leading_zero == 0)
+        {
+            buf[len] = t + '0';
+            len += 1;
+        }
+        b = b / 10;
+    }
+    return len;
+}
+
+static int safe_printf(const char *fmt, ...)
+{
+    va_list argp;
+    va_start(argp, fmt);
+
+    // 2 pointers:
+    // i for scanned in format string
+    // j for written in stdout
+    // write for: (1) string in fmt; (2) escape in fmt; in each loop
+    int i = 0, j = 0;
+    int start = 0, len = 0;
+    while (fmt[i] != '\0')
+    {
+        // Handle format characters
+        if (fmt[i] == '%')
+        {
+            switch (fmt[i + 1])
+            {
+            // string format
+            case 's':
+                // write previous string in fmt
+                write(STDOUT_FILENO, &fmt[start], i - start);
+                // write string in arg
+                char *str = va_arg(argp, char *);
+                len = strlen(str);
+                write(STDOUT_FILENO, str, len);
+                // reset pointer
+                i += 2;
+                start = i;
+                j += len;
+                break;
+
+            // unsigned format
+            case 'u':
+                // write previous string in fmt
+                write(STDOUT_FILENO, &fmt[start], i - start);
+                // write unsigned in arg
+                char buf[11];
+                unsigned u = va_arg(argp, unsigned);
+                len = convert_unsigned(u, buf);
+                write(STDOUT_FILENO, buf, len);
+                // reset pointer
+                i += 2;
+                start = i;
+                j += len;
+                break;
+            
+            default:
+                // like continue
+                i += 1;
+                j += 1;
+                break;
+            }
+        }
+        else
+        {
+            i += 1;
+            j += 1;
+        }
+    }
+    // print the tailing strings
+    if (i - start > 0)
+    {
+        write(STDOUT_FILENO, &fmt[start], i - start);
+    }
+
+    va_end(argp);
+    return j;
 }
